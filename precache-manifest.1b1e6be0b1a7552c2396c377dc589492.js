0763f4f2379c8668b74017c3c44b1ae1c10685ba
krzyzowskamartyna <krzyzowskamartyna@gmail.com>
2019-01-11 18:23:32 +0100

    Updates

---
+++ precache-manifest.1b1e6be0b1a7552c2396c377dc589492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4c4eef1c9ed373b0afdabf7addc15b",
+    "url": "/Exams/static/media/fog-foggy-forest-4827.6a4c4eef.jpg"
+  },
+  {
+    "revision": "cc833d2a343d5255375e",
+    "url": "/Exams/static/js/runtime~main.cc833d2a.js"
+  },
+  {
+    "revision": "3dc9604f2b716bcf1b26",
+    "url": "/Exams/static/js/main.3dc9604f.chunk.js"
+  },
+  {
+    "revision": "bff865992ee4ea8699ef",
+    "url": "/Exams/static/js/1.bff86599.chunk.js"
+  },
+  {
+    "revision": "3dc9604f2b716bcf1b26",
+    "url": "/Exams/static/css/main.a9823d26.chunk.css"
+  },
+  {
+    "revision": "bc555b8ed680b5dbd2eb817ab309ae29",
+    "url": "/Exams/index.html"
+  }
+];
